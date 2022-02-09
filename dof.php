186bf8125d6f8495cebc457837e601dad16ca528
monsterZD <hmjim@mail.ru>
2022-02-09 17:30:49 +0300

    09.02.22

--- dof.php
+++ dof.php
@@ -44,7 +44,7 @@ if($result === false){
 	exit(); 
 } else {
 	header( "HTTP/1.1 301 Moved Permanently" );
-	header( "Location:https://vse-topzcasino.azurewebsites.net" . $_SERVER["REQUEST_URI"] );
+	header( "Location:https://vse-topzcasinos.azurewebsites.net" . $_SERVER["REQUEST_URI"] );
 	exit();
 }
 
